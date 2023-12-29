65560cada1165f9d570c3b778ca00b880f00b730
Ankush169 <137650329+Ankush169@users.noreply.github.com>
2023-12-30 01:16:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+import joblib
+import pandas as pd
+import numpy as np
+print("Ankush1")
+
+app = Flask(__name__)
+
+#mul_reg = open("multiple_regression_model.pkl", "rb")
+#ml_model = joblib.load(mul_reg)
+
+print("Ankush2")
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+print("Ankush3")
+
+@app.route("/predict", methods=['GET', 'POST'])
+def predict():
+    print("I was here 1")
+    if request.method == 'POST':
+        print(request.form.get('NewYork'))
+        try:
+            NewYork = float(request.form['NewYork'])
+            California = float(request.form['California'])
+            Florida = float(request.form['Florida'])
+            RnD_Spend = float(request.form['RnD_Spend'])
+            Admin_Spend = float(request.form['Admin_Spend'])
+            Market_Spend = float(request.form['Market_Spend'])
+            pred_args = [NewYork, California, Florida, RnD_Spend, Admin_Spend, Market_Spend]
+            pred_args_arr = np.array(pred_args)
+            pred_args_arr = pred_args_arr.reshape(1, -1)
+            mul_reg = open("multiple_regression_model.pkl", "rb")
+            ml_model = joblib.load(mul_reg)
+            model_prediction = ml_model.predict(pred_args_arr)
+            model_prediction = round(float(model_prediction), 2)
+        except ValueError:
+            return "Please check if the values are entered correctly"
+    return render_template('predict.html', prediction = model_prediction)
+print("Ankush4")
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0', port=5001)
